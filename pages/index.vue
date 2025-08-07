77447806769a43fab7406194f2dab2bc4142b4c0
o6ep9 <oberd.alina@gmail.com>
2025-08-07 23:17:03 +0300

    п

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>
+      Welcome to Olivence
+    </h1>
+  </div>
+</template>
+
+<script setup>
+// o
+</script>
+
+<style>
+html, body {
+  margin: 0;
+  padding: 0;
+}
+</style>
